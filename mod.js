b95870f032e680d543d5fafe0436433c643bda05
Creeplays <f6cf@fccccc.cf>
2016-01-09 14:10:54 +0000

    new compiler

--- mod.js
+++ mod.js
@@ -1,13 +1,17 @@
 var connect = (emitter, config)=>{
     emitter.on("modLoaded",()=>{
-        emitter.emit("clientMessage",{
-            message:`&1It &2works!\n${config}\n&4test!`
-        });
+        //TODO: Need to use internals
+        //emitter.emit("clientMessage",{
+            //message:`&1It &2works!\n${config}\n&4test!`
+        //});
+        internals.clientMessage(`&1It &2works!\n${config}\n&4test!`);
     });
-    emitter.on("clientMessage",(msg)=>{
-        print(`You can even hook clientMessage!\n${msg}`);
+    emitter.on("local.anonymous.chat",(msg)=>{
+        //Hehe, TODO: Need to use internals
+        //print(`You can even hook clientMessage!\n${msg}`);
+        print("useless hook, this compiler is beta");
     });
-    emitter.on("levelLoad",()=>{
+    emitter.on("level.load.end",()=>{
         print("Level loaded! Power in ECMASCRIPT6, luke!");
     });
 };
